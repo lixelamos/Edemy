a8843ea52cd01b2c570f2cff3277d84b2ec2b2f2
Lixel Amos <48270069+lixelamos@users.noreply.github.com>
2025-06-11 10:16:41 +0300

    deploy2

--- client/src/pages/student/MyEnrollMents.jsx
+++ client/src/pages/student/MyEnrollMents.jsx
@@ -25,7 +25,7 @@ const MyEnrollments = () => {
 
 			const tempProgressArray = await Promise.all(
 				enrolledCourses.map(async (course) => {
-					const { data } = await axios.post(
+					const response = await axios.post(
 						`${backendUrl}/api/user/get-course-progress`,
 						{ courseId: course._id },
 						{ headers: { Authorization: `Bearer ${token}` } }
@@ -33,8 +33,8 @@ const MyEnrollments = () => {
 					
 					let totalLectures = calculateNoOfLectures(course);
 
-					const lectureCompleted = data.progressData
-						? data.progressData.lectureCompleted.length
+					const lectureCompleted = response.data.progressData
+						? response.data.progressData.lectureCompleted.length
 						: 0;
 					return { totalLectures, lectureCompleted };
 				})
